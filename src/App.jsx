4537f836cf1f08f5f7cc13b6b1b6677d93ab26d9
Priya Prakash <priyprakashpandey@gmail.com>
2023-06-01 20:34:47 +0530

    First commit Frontend Complete

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import About from "./components/About"
+import Contect from "./components/Contect"
+import HireMe from "./components/HireMe"
+import Navbar from "./components/Navbar"
+import Resume from "./components/Resume"
+
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <About />
+      <Resume />
+      <HireMe />
+      <Contect />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Contect.jsx
@@ -0,0 +1,124 @@
+import React from "react";
+
+export default function Contect() {
+  return (
+    <div class="section contact" id="contact" style={{marginTop:"2vw"}}>
+      <div id="map" class="map">
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14643.78029088155!2d83.98006679307211!3d25.56208379213321!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3991c8a4e32aaf83%3A0xb2529e9a8132dd52!2sBuxar%2C%20Bihar!5e0!3m2!1sen!2sin!4v1622572402930!5m2!1sen!2sin"
+          width={1513}
+          height="700"
+          loading="lazy"
+        ></iframe>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-8">
+            <div class="contact-form-card">
+              <h4 class="contact-title">Send a message</h4>
+              <form action="">
+                <div class="form-group">
+                  <input
+                    class="form-control"
+                    type="text"
+                    placeholder="Name *"
+                    required
+                  />
+                </div>
+                <div class="form-group">
+                  <input
+                    class="form-control"
+                    type="email"
+                    placeholder="Email *"
+                    required
+                  />
+                </div>
+                <div class="form-group">
+                  <textarea
+                    class="form-control"
+                    id=""
+                    placeholder="Message *"
+                    rows="7"
+                    required
+                  ></textarea>
+                </div>
+                <div class="form-group ">
+                  <button type="submit" class="form-control btn btn-primary">
+                    Send Message
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+          <div class="col-lg-4">
+            <div class="contact-info-card">
+              <h4 class="contact-title">Get in touch</h4>
+              <div class="row mb-2">
+                <div class="col-1 pt-1 mr-1">
+                  <i class="ti-mobile icon-md"></i>
+                </div>
+                <div class="col-10 ">
+                  <h6 class="d-inline">
+                    Phone : <br />{" "}
+                    <span class="text-muted">+ (123) 456-789</span>
+                  </h6>
+                </div>
+              </div>
+              <div class="row mb-2">
+                <div class="col-1 pt-1 mr-1">
+                  <i class="ti-map-alt icon-md"></i>
+                </div>
+                <div class="col-10">
+                  <h6 class="d-inline">
+                    Address :<br />{" "}
+                    <span class="text-muted">
+                      12345 Fake ST NoWhere AB Country.
+                    </span>
+                  </h6>
+                </div>
+              </div>
+              <div class="row mb-2">
+                <div class="col-1 pt-1 mr-1">
+                  <i class="ti-envelope icon-md"></i>
+                </div>
+                <div class="col-10">
+                  <h6 class="d-inline">
+                    Email :<br />{" "}
+                    <span class="text-muted">info@website.com</span>
+                  </h6>
+                </div>
+              </div>
+              <ul class="social-icons pt-4">
+                <li class="social-item">
+                  <a class="social-link text-dark" href="#">
+                    <i class="ti-facebook" aria-hidden="true"></i>
+                  </a>
+                </li>
+                <li class="social-item">
+                  <a class="social-link text-dark" href="#">
+                    <i class="ti-twitter" aria-hidden="true"></i>
+                  </a>
+                </li>
+                <li class="social-item">
+                  <a class="social-link text-dark" href="#">
+                    <i class="ti-google" aria-hidden="true"></i>
+                  </a>
+                </li>
+                <li class="social-item">
+                  <a class="social-link text-dark" href="#">
+                    <i class="ti-instagram" aria-hidden="true"></i>
+                  </a>
+                </li>
+                <li class="social-item">
+                  <a class="social-link text-dark" href="#">
+                    <i class="ti-github" aria-hidden="true"></i>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
